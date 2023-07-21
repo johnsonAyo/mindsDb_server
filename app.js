5356d34cc9bb25587c606c5fe3b798b5c8813d5c
ambiti0n <fabulousfresh010@gmail.com>
2023-07-21 14:50:56 +0100

    chore: api welcome msg

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = process.env.PORT;
 
 const mindDbQueryCall = async (authorUsername, searchText) => {
   try {
-    // Connect and get the session token
     const loginResponse = await axios.post(
       "https://cloud.mindsdb.com/cloud/login",
       {
@@ -30,7 +29,6 @@ const mindDbQueryCall = async (authorUsername, searchText) => {
       sessionToken = sessionCookie.substring(startIndex, sessionCookie.length);
     }
 
-    // Set the session token in the headers for subsequent requests
     const axiosInstance = axios.create({
       headers: {
         Accept: "application/json",
@@ -41,7 +39,6 @@ const mindDbQueryCall = async (authorUsername, searchText) => {
 
     const query = `SELECT * FROM mindsdb.mindsdb.tutor_model WHERE author_username = '${authorUsername}' AND text = '${searchText}';`;
 
-    // Query the API
     const response = await axiosInstance.post(
       "https://cloud.mindsdb.com/api/sql/query",
       {
@@ -55,24 +52,50 @@ const mindDbQueryCall = async (authorUsername, searchText) => {
   }
 };
 
-// Express route
+app.get("/", async (req, res) => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Welcome to the MindDB Language Tutor API</title>
+    </head>
+    <body>
+        <div style="text-align: center; margin-top: 100px;">
+            <h1>Welcome to the MindDB Language Tutor API</h1>
+            <p>Explore the realm of languages, where knowledge blooms,</p>
+            <p>Amidst the digital dreams, where wisdom looms.</p>
+            <p>Embark on a journey, where words come alive,</p>
+            <p>In the magical dance of intellect, you shall thrive.</p>
+            <p>Step forth to the gateway, with hearts bold and keen,</p>
+            <p>Where the chat-box awaits, in this mystical scene.</p>
+            <p>Through the API's embrace, the quest shall ignite,</p>
+            <p>As understanding and learning take flight.</p>
+            <p>So let the adventure begin, with minds open wide,</p>
+            <p>In the MindDB Language Tutor, where wisdom resides.</p>
+            <br>
+            <p>Visit <code>app.post("/api/chat-box", async (req, res)</code> to start</p>
+        </div>
+    </body>
+    </html>
+  `;
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.status(500).send(htmlContent);
+});
+
 app.post("/api/chat-box", async (req, res) => {
   try {
-    // Get searchText and authorUsername from req.body
     const { searchText, authorUsername } = req.body;
 
-    // Perform the API call
     const data = await mindDbQueryCall(authorUsername, searchText);
 
-    // Send the response
     res.json(data);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Something went wrong!" });
   }
 });
-
-// Start the server
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
